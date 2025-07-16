c03a1474286fb5e05eab6372310910600db044e1
Aman <amanfarhan2008@gmail.com>
2025-07-16 21:52:36 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// let firstName = ["Irfan" , "mubeen", "zaheer" , "khizer"];
+// let lastName = ["khan" , "UL Islam", "Ahmed" , "Sajjad"];
+
+// for (i = 0 ; i < firstName.length ; i++){
+//     for (let j = 0 ; j < lastName.length ; j++){
+//         console.log(firstName[i] + " " + lastName[i])
+//     }
+// }
+
+// let firstName = ["Irfan" , "mubeen", "zaheer" , "khizer"];
+// let lastName = ["khan" , "UL Islam", "Ahmed" , "Sajjad"];
+
+// for (i = 0 ; i < firstName.length; i++ ){
+//     for (j = 0 ; j < lastName.length ; j++){
+//         console.log(firstName[i]+ " "+ lastName[i]);
+//     } 
+// }
+
+
+// let rounds = ["round 1","round2",]
+
+// for (i = 0 ; i < 5 ; i++){
+//     console.log("you have completed "+ i + "number or rounds");
+//     for (j = 0 ; j < 5 ; j++){
+//     console.log("you have done " + j + "glasses of water");
+//     } for (k = 0 ; k < )
+// }
+
+
+// for (i = 0 ; i < 3 ; i++){
+//     let row = "";
+//     for (j = 0 ; j < 3 ; j++){
+//         row = row + "* " ;
+//     }
+//     console.log(row , i)
+// }
+
+// let num = 5
+// for (i = 1 ; i <= 10 ; i++){
+//     console.log(num +"x" + i + " = " + num*i );
+
