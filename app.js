82c0f187065853784018d36a49b9dd88e7f22527
Aman <amanfarhan2008@gmail.com>
2025-07-17 23:06:39 +0500

    yesh

--- app.js
+++ app.js
@@ -35,10 +35,10 @@
 //     console.log(row , i)
 // }
 
-// let num = 5
-// for (i = 1 ; i <= 10 ; i++){
-//     console.log(num +"x" + i + " = " + num*i );
-// }
+let num = 5
+for (i = 1 ; i <= 10 ; i++){
+    console.log(num +"x" + i + " = " + num*i );
+}
 
 
 
